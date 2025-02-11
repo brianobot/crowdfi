b1636e243576f711d977805f83234da2b04e1041
Brian Obot <brianobot@gmail.com>
2025-02-11 08:41:12 +0100

    Refactor initialize function to accept max_amount and max_duration parameters

--- programs/crowdfi/src/lib.rs
+++ programs/crowdfi/src/lib.rs
@@ -4,17 +4,17 @@ pub mod state;
 pub mod contexts;
 pub mod errors;
 
+pub use contexts::*;
+
+
 declare_id!("5msKK8UEpEJLyT8df2rQH76v9UzTdRkfrZ1cgHNE7647");
 
 #[program]
 pub mod crowdfi {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(ctx: Context<Initialize>, max_amount: u64, max_duration: u64) -> Result<()> {
+        ctx.accounts.init(max_amount, max_duration, &ctx.bumps)?;
         Ok(())
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
