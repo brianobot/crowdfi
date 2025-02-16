9628ff5969d7f4c39a23135109f4d72aeb4cc827
Brian Obot <brianobot@gmail.com>
2025-02-16 10:30:24 +0100

    Enhance campaign management by adding validation for title, description, and URL lengths; implement update functionality for campaign details; and introduce error handling for campaign constraints.

--- programs/crowdfi/src/lib.rs
+++ programs/crowdfi/src/lib.rs
@@ -16,13 +16,18 @@ declare_id!("5msKK8UEpEJLyT8df2rQH76v9UzTdRkfrZ1cgHNE7647");
 pub mod crowdfi {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>, max_amount: u64, max_duration: u64) -> Result<()> {
-        ctx.accounts.init(max_amount, max_duration, &ctx.bumps)?;
+    pub fn initialize(ctx: Context<Initialize>, seed: u8, max_amount: u64, max_duration: u64) -> Result<()> {
+        ctx.accounts.init(seed, max_amount, max_duration, &ctx.bumps)?;
         Ok(())
     }
 
-    pub fn create_campaign(ctx: Context<CreateCampaign>) -> Result<()> {
-        ctx.accounts.init_campaign()?;
+    pub fn create_campaign(ctx: Context<CreateCampaign>, title: String, description: String, url: String, target_amount: u64, start_timestamp: u64, end_timestamp: u64) -> Result<()> {
+        ctx.accounts.init(title, description, url, target_amount, start_timestamp, end_timestamp, &ctx.bumps)?;
+        Ok(())
+    }
+
+    pub fn update_campaign(ctx: Context<UpdateCampaign>, description: Option<String>, url: Option<String>) -> Result<()> {
+        ctx.accounts.update(description, url)?;
         Ok(())
     }
 }
